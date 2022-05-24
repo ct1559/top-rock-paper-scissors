6b24ca923d8d254b171fd50ea85a6a436f36be35
ct1559 <e.cooper.taylor@gmail.com>
2022-05-24 13:44:41 -0400

    Create play round function
    Play round function takes player selection and computer
    selection as parameters. Take player argument and
    make first letter capital and the rest lower. Compare
    human player input with computer input to decide a
    winner.

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 // Create function that makes a play by the computer
 function computerPlay() {
   // Create variable for possible computer choices
-  let choiceArr = ["Rock", "Paper", "Scissors"];
+  const choiceArr = ["Rock", "Paper", "Scissors"];
 
   // Randomly pick one of the 3 variables
   let npcChoice = Math.floor(Math.random() * 3);
   return choiceArr[npcChoice];
 }
+
+function playRound(playerSelection, computerSelection) {
+  // Change player input to first letter capital, rest lowercase
+  let playerSelectionArr = playerSelection.split("");
+  playerSelectionArr[0] = playerSelectionArr[0].toUpperCase();
+  for (let i = 1; i < playerSelectionArr.length; i++) {
+    playerSelectionArr[i] = playerSelectionArr[i].toLowerCase();
+  }
+
+  const playerSelectionFixed = playerSelectionArr.join("");
+
+  // Functions for winning and losing
+  let winRound = () =>
+    `You win! ${playerSelectionFixed} beats ${computerSelection}`;
+  let loseRound = () =>
+    `You lose! ${computerSelection} beats ${playerSelectionFixed}`;
+
+  // When both make the same choice
+  if (playerSelectionFixed === computerSelection) {
+    return `It's a draw! Both chose ${computerSelection}`;
+  } else {
+    //All other possiblilities
+    switch (playerSelectionFixed) {
+      case "Rock":
+        if (computerSelection === "Scissors") {
+          return winRound();
+        } else {
+          return loseRound();
+        }
+      case "Paper":
+        if (computerSelection === "Rock") {
+          return winRound();
+        } else {
+          return loseRound();
+        }
+      case "Scissors":
+        if (computerSelection === "Paper") {
+          return winRound();
+        } else {
+          return loseRound();
+        }
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
